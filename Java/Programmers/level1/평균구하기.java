83147b0aa783dbf585df3dcb2fe75518cc1b513a
omnireader0 <omnireader0@gmail.com>
2021-12-30 23:16:13 +0900

    code : 21-12-30 PRG L1 평균구하기

---
+++ Java/Programmers/level1/평균구하기.java
@@ -0,0 +1,19 @@
+package Java.Programmers.level1;
+
+public class 평균구하기 {
+    // 버전 1
+    // public static void main(String [] args){
+    //     평균구하기 s = new 평균구하기();
+    //     int [] arr = {1,2,3,4};
+    //     System.out.println(s.solution(arr));
+    // }
+    // public double solution(int[] arr){
+    //     int sum=0;
+    //     for (int a : arr){
+    //         sum+= a;
+    //     }
+    //     return (double)sum/arr.length;
+    // }
+
+ 
+}
