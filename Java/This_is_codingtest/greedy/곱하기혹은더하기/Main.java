bd438891f4e248ff8cfb604d444902a042939aff
omnireader0 <omnireader0@gmail.com>
2022-01-24 23:39:41 +0900

    ADD : 22-01-24 이코테 곱하기혹은더하기, java

---
+++ Java/This_is_codingtest/greedy/곱하기혹은더하기/Main.java
@@ -0,0 +1,23 @@
+package This_is_codingtest.곱하기혹은더하기;
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        
+        // long : string이 사이즈가 20인 경우, 숫자로 변환시 커짐
+        long answer = str.charAt(0) - '0';
+
+        for (int i=1; i<str.length(); i++) {
+            int num = str.charAt(i) - '0';
+            if (num <= 1 || answer <= 1) {
+                answer += num;
+            }
+            else{
+                answer *= num;
+            }
+        }
+        System.out.println(answer);
+    }
+}
