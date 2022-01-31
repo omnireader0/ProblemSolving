f149b1a0f3a6fb1a0e01d92342116568ca11b124
omnireader0 <omnireader0@gmail.com>
2022-01-31 22:52:31 +0900

    add : 21-01-31 PRG 문자열압축 , python java

---
+++ Java/This_is_codingtest/implementation/PRG_문자열압축/Main.java
@@ -0,0 +1,5 @@
+package Java.This_is_codingtest.implementation.PRG_문자열압축;
+
+public class Main {
+    
+}
---
+++ Python/This is codingtest/implementation/PRG_문자열압축/문자열압축.py
@@ -0,0 +1,22 @@
+# def solution(s):
+#     answer = len(s)
+    
+#     for i in range(1, len(s)//2+1):
+#         comp = ""
+#         cnt = 1
+#         prev = s[:i]
+#         for j in range(i, len(s)+i, i):
+#             if s[j:j+i] == prev:
+#                 cnt += 1
+#             else:
+#                 if cnt == 1:
+#                     comp += prev
+#                 else:
+#                     comp += str(cnt) + prev
+#                 prev = s[j:j+i]
+#                 cnt = 1
+#         answer = min(answer, len(comp))
+#     return answer
+                
+s = 'sdfsdfe'
+print(s[6:8])
