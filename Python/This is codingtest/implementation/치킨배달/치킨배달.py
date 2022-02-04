6fa27a66acdad30605d468547cc3ce358366b7d9
omnireader0 <omnireader0@gmail.com>
2022-02-04 11:09:33 +0900

    add : 22-04-02 BOJ G5 15686 치킨배달 , python

---
+++ Python/This is codingtest/implementation/치킨배달/치킨배달.py
@@ -0,0 +1,27 @@
+from itertools import combinations
+    
+n, m = map(int, input().split())
+map = list(list(map(int, input().split())) for _ in range(n))
+
+chickens = []
+home = []
+for i in range(n):
+    for j in range(n):
+        if map[i][j] == 2:
+            chickens.append([i, j])
+        if map[i][j] == 1:
+            home.append([i, j])
+
+chicken_combi = list(combinations(chickens, m))
+
+answer = 1e9
+for combi in chicken_combi:
+    result = 0
+    for x, y in home:
+        min_ = 100
+        for a, b in combi:
+            min_ = min(min_, abs(x-a)+abs(y-b))
+        result += min_
+    answer = min(answer, result)
+
+print(answer)
