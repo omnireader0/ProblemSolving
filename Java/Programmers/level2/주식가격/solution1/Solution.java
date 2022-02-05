123b1fb71a01cac731e77d49eea3a9b02969d276
omnireader0 <omnireader0@gmail.com>
2022-02-05 23:50:18 +0900

    add : 22-02-05 PRG level2 주식가격

---
+++ Java/Programmers/level2/주식가격/solution1/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        int size = prices.length;
+        for (int i=0; i<size; i++) {
+            int cnt = 0;
+            for (int j=i+1; j<size; j++){
+                cnt++;
+                if (prices[i] > prices[j]){
+                    break;
+                }
+            }
+            answer[i] = cnt;
+        }
+        return answer;
+    }
+}
