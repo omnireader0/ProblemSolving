0c62942f6a9ffcb2cd0a7c212752ffa6b89f507c
omnireader0 <omnireader0@gmail.com>
2022-02-05 23:49:09 +0900

    add : 22-02-05 PRG level2 스킬트리

---
+++ Java/Programmers/level2/스킬트리/solution1/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+        for(String skills : skill_trees){
+            int idx = 0;
+            boolean flag = true;
+            for(int i=0; i<skills.length(); i++){
+                if(skills.charAt(i) == skill.charAt(idx)){
+                    idx++;
+                    if(idx>=skill.length())
+                        break;
+                }
+                else if(skill.contains(Character.toString(skills.charAt(i))))
+                    flag = false;
+            }
+            if(flag)
+                answer++;
+        }
+        return answer;
+    }
+}
