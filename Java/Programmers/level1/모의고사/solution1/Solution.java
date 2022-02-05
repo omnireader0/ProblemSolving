9187d2969bddf4963504f9be274999a157fb4f7a
omnireader0 <omnireader0@gmail.com>
2022-02-05 23:44:21 +0900

    add : 22-02-05 PRG level1 모의고사

---
+++ Java/Programmers/level1/모의고사/solution1/Solution.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] answers) {
+        List<Integer> result = new ArrayList<Integer> ();
+        int[] number1 = {1, 2, 3, 4, 5};
+        int[] number2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] number3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int cnt1 = 0, cnt2 = 0, cnt3 = 0;
+        for(int i=0; i<answers.length; i++){
+            if(answers[i] == number1[i%5]){
+                cnt1 ++;
+            }
+            if(answers[i] == number2[i%8]){
+                cnt2 ++;
+            }
+            if(answers[i] == number3[i%10]){
+                cnt3 ++;
+            }
+        }
+        int maxCnt = (cnt1>cnt2)&&(cnt1>cnt3)?cnt1:(cnt3>cnt2?cnt3:cnt2);
+        if (maxCnt==cnt1) result.add(1);
+        if (maxCnt==cnt2) result.add(2);
+        if (maxCnt==cnt3) result.add(3);
+
+        int [] answer= new int[result.size()];
+        for(int i=0; i<result.size(); i++){
+            answer[i] = result.get(i);
+        }
+        return answer;
+    }
+}
---
+++ Java/Programmers/level1/모의고사/solution2/Solution.java
@@ -0,0 +1,56 @@
+import java.util.*;
+class Solution {
+    
+    class Student {
+        private int[] answers;
+        private int cnt;
+        private int id = 0;
+        
+        public Student(int id, int[] answers) {
+            this.answers = answers;
+            this.cnt = 0;
+            this.id = id;
+        }
+        
+        public void matchAnswer(int[] matchAnswers) {
+            for (int i=0; i<matchAnswers.length; i++) {
+                if (matchAnswers[i] == answers[i % answers.length])
+                    cnt++;
+            }
+        }
+        
+        public int getId() {
+            return id;
+        }
+        
+        public int getCnt() {
+            return cnt;
+        }
+        
+    }
+    
+    public int[] solution(int[] answers) {
+        
+        Student[] students = {
+            new Student(1, new int[]{1, 2, 3, 4, 5}),
+            new Student(2, new int[]{2, 1, 2, 3, 2, 4, 2, 5}),
+            new Student(3, new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5})
+        };
+
+        int max = Integer.MIN_VALUE;
+        
+        for (int i=0; i<students.length; i++) {
+            students[i].matchAnswer(answers);
+            max = Math.max(max, students[i].getCnt());
+        }
+        
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        
+        for (int i=0; i<students.length; i++) {
+            if (students[i].getCnt() == max)
+                list.add(students[i].getId());
+        }
+
+        return list.stream().mapToInt(i->i).toArray();
+    }
+}
