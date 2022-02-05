1e9d91209c4b56a7087ea9d334bb2fdb8999576e
omnireader0 <omnireader0@gmail.com>
2022-02-05 23:48:38 +0900

    add : 22-02-05 PRG level2 더맵게

---
+++ Java/Programmers/level2/더맵게/solution1/Solution.java
@@ -0,0 +1,20 @@
+import java.util.PriorityQueue;
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0; 
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int a : scoville) {
+            pq.offer(a);
+        }
+
+        while (pq.size() > 1 && pq.peek() < K) {
+            int first = pq.poll();
+            int second = pq.poll();
+            pq.offer(first+(second*2));
+            answer++;
+        }
+
+        return pq.peek() >= K ? answer : -1;
+    }
+}
