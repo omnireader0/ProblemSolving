2bd46c6b94efb3b0a74db5d0c6852c0f11bc5f13
omnireader0 <omnireader0@gmail.com>
2022-02-05 23:52:25 +0900

    add : 22-02-05 PRG level4 도둑질

---
+++ Java/Programmers/level4/도둑질/solution1/Solution.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] money) {
+        int length = money.length;
+        int[] dp1 = new int[length]; 
+        int[] dp2 = new int[length];
+        
+        // 첫 번째 집 털고, 두 번째 집 안터는 경우
+        dp1[0] = money[0];
+        dp1[1] = dp1[0];
+        
+        // 두 번째 집 털고, 첫 번째 집 안터는 경우
+        dp2[0] = 0;
+        dp2[1] = money[1];
+        
+        for(int i = 2; i < length; i++) {
+            
+            dp2[i] = Math.max(dp2[i-2]+money[i], dp2[i-1]);
+            
+            // 첫번째 집 터는 경우에는 마지막 집을 털면 안되므로
+            if (i == length-1) continue;
+            dp1[i] = Math.max(dp1[i-2]+money[i], dp1[i-1]);
+            
+        }
+        return Math.max(dp1[length-2], dp2[length-1]);
+    }
+}
