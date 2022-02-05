f27e2943a0bee0993a3c3345a560197ef85d8a88
omnireader0 <omnireader0@gmail.com>
2022-02-05 23:50:29 +0900

    add : 22-02-05 PRG level2 타겟넘버

---
+++ Java/Programmers/level2/타겟넘버/solution1/Solution.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] numbers, int target) {
+        List<Integer> answer = new ArrayList<>();
+        answer.add(0);
+        for (int i=0; i<numbers.length; i++) {
+            List<Integer> node = new ArrayList<>();
+            for (int j=0; j<answer.size(); j++) {
+                node.add(answer.get(j) - numbers[i]);
+                node.add(answer.get(j) + numbers[i]);
+            }
+            answer = node;
+        }
+        int count = 0;
+        for (int i: answer) {
+            if (i==target) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
