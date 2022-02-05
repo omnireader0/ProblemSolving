1ff69a833559c93b8e4f47818c4a149b817f73be
omnireader0 <omnireader0@gmail.com>
2022-02-05 23:45:35 +0900

    add : 22-02-05 PRG level1 체육복

---
+++ Java/Programmers/level1/체육복/solution2/Solution.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int cnt = 0;
+        int[] students = new int[n+1];
+        Arrays.fill(students, 1); 
+
+        for (int i : reserve) students[i]++;
+        for (int i : lost) students[i]--;
+
+        for (int i = 1; i <= n; i++) {
+            if (students[i] > 0) continue;
+
+            if (students[i-1] > 1) {
+                students[i-1]--;
+                students[i]++;
+                continue;
+            }
+            
+            if(i < n && students[i+1] > 1) {
+                students[i+1]--;
+                students[i]++;
+            }
+        }
+        // 다른 방법 
+        // return (int) Arrays.stream(students).filter(i -> i>0).count();
+
+        for (int i = 1; i <= n; i++) {
+            if (students[i] > 0) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
