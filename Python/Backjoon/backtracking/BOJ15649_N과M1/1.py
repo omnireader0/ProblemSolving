9a70ca07f2ec0057feb93a0546bb1e0c3a76cd91
omnireader0 <omnireader0@gmail.com>
2022-05-07 13:39:58 +0900

    add : 22-05-07 BOJ 15649

--- Python/Backjoon/backtracking/BOJ15649_N과M1/1.py
+++ Python/Backjoon/backtracking/BOJ15649_N과M1/1.py
@@ -16,3 +16,18 @@ dfs()
 
 # 전체 반복문에서 dfs 통해 작은 반복문을 돌게 됨
 # 작은 반복문 완료되면 전체 반복문에서 남은 작업 처리해주기
+
+'''
+n,m = map(int, input().split())
+arr = []
+def dfs():
+    if len(arr) == m:
+        print(' '.join(map(str, arr)))
+        return
+    for i in range(1, n+1):
+        if i not in arr:
+            arr.append(i)
+            dfs()
+            arr.pop()
+dfs()
+'''
