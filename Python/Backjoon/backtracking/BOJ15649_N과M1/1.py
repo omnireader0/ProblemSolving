b8dc9261fba424d805670a1e8f9d169bfd0ddd6e
omnireader0 <omnireader0@gmail.com>
2022-05-26 23:19:23 +0900

    add : 22-05-26 15649 복습

--- Python/Backjoon/backtracking/BOJ15649_N과M1/1.py
+++ Python/Backjoon/backtracking/BOJ15649_N과M1/1.py
@@ -1,22 +1,5 @@
-n,m = map(int, input().split())
-arr = []
-def dfs():
-    if len(arr) == m:
-        print(' '.join(map(str, arr)))
-        return
-    for i in range(1, n+1):
-        if i not in arr:
-            arr.append(i)
-            print(arr, '+++')
-            dfs()
-            print(arr, '---')
-            arr.pop()
-            print(arr, '~~~')
-dfs()
-
 # 전체 반복문에서 dfs 통해 작은 반복문을 돌게 됨
 # 작은 반복문 완료되면 전체 반복문에서 남은 작업 처리해주기
-
 '''
 n,m = map(int, input().split())
 arr = []
@@ -31,3 +14,24 @@ def dfs():
             arr.pop()
 dfs()
 '''
+
+n,m = map(int, input().split())
+v = [0]*8
+visited = [0]*9
+
+def dfs(depth):
+    
+    if depth == m:
+        for i in range(m):
+            print(v[i], end = ' ')
+        print()
+        return
+    
+    for i in range(1, n+1):
+        if visited[i] == 0:
+            visited[i] = 1
+            v[depth] = i
+            dfs(depth + 1)
+            visited[i] = 0    
+
+dfs(0)
