28a6aed00758a58ad1071d9316c5bf59afab73c0
omnireader0 <omnireader0@gmail.com>
2022-05-27 23:55:33 +0900

    add : 22-05-27 boj 15659

--- Python/Backjoon/dfsbfs/BOJ15659_연산자끼워넣기3/1.py
+++ Python/Backjoon/dfsbfs/BOJ15659_연산자끼워넣기3/1.py
@@ -1,47 +1,37 @@
-'''
-순열을 구해야 한다. 
-'''
-
-# n = int(input())
-# data = list(map(int, input().split()))
-# plus, minus, mul, div = map(int, input().split())
-# max_ , min_ = -1e9, 1e9
-
-
 import sys
-input = sys.stdin.readline
-
-def dfs(expression, depth, use_cnt):
-    global max_result, min_result
-    if depth == N:
-        tmp = eval(expression)
-        max_result = max(max_result, tmp)
-        min_result = min(min_result, tmp)
-        return
-    if use_cnt[0] < p_cnt:
-        use_cnt[0] += 1
-        dfs(expression + "+" + str(nums[depth]), depth + 1, use_cnt)
-        use_cnt[0] -= 1
-    if use_cnt[1] < mi_cnt:
-        use_cnt[1] += 1
-        dfs(expression + "-" + str(nums[depth]), depth + 1, use_cnt)
-        use_cnt[1] -= 1
+input = sys.stdin.readline 
  
-    if use_cnt[2] < mu_cnt:
-        use_cnt[2] += 1
-        dfs(expression + "*" + str(nums[depth]), depth + 1, use_cnt)
-        use_cnt[2] -= 1
- 
-    if use_cnt[3] < d_cnt:
-        use_cnt[3] += 1
-        dfs(expression + "//" + str(nums[depth]), depth + 1, use_cnt)
-        use_cnt[3] -= 1
- 
- 
-N = int(input())
+n = int(input())
 nums = list(map(int, input().split()))
-p_cnt, mi_cnt, mu_cnt, d_cnt = map(int, input().split())
-max_result, min_result = -float('inf'), float('inf')
-dfs(str(nums[0]), 1, [0, 0, 0, 0])
-print(max_result)
-print(min_result)
+oper = list(map(int, input().split()))
+max_, min_ = -1e9, 1e9
+
+def dfs(value, cnt):
+    global oper, max_, min_
+    if cnt == n:
+        temp = eval(value)
+        max_ = max(temp, max_)
+        min_ = min(temp, min_)
+        return 
+        
+    if oper[0] > 0:
+        oper[0] -= 1
+        dfs(value + "+" + str(nums[cnt]), cnt+1)
+        oper[0] += 1
+    if oper[1] > 0:
+        oper[1] -= 1
+        dfs(value + "-" + str(nums[cnt]), cnt+1)
+        oper[1] += 1
+    if oper[2] > 0:
+        oper[2] -= 1
+        dfs(value + "*" + str(nums[cnt]), cnt+1)
+        oper[2] += 1
+    if oper[3] > 0:
+        oper[3] -= 1
+        dfs(value + "//" + str(nums[cnt]), cnt+1)
+        oper[3] += 1
+
+dfs(str(nums[0]), 1)
+print(max_)
+print(min_)
+
