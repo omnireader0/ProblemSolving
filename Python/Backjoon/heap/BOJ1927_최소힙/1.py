128715770a1b4af5713b2d98cf8a4ec1bf9228b0
omnireader0 <omnireader0@gmail.com>
2022-05-27 23:54:40 +0900

    add : 22-05-27 boj 1927

---
+++ Python/Backjoon/heap/BOJ1927_최소힙/1.py
@@ -0,0 +1,17 @@
+import heapq
+import sys
+input = sys.stdin.readline
+heap = []
+for i in range(int(input())):
+    x = int(input())
+    
+    if x == 0:
+        if len(heap) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(heap))    
+    else:
+        heapq.heappush(heap, x)
+        
+        
+        
